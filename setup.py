1208718568c8943366b58428156acd826c2f91b6
Charanraj Shetty <charanrajshetty1993@gmail.com>
2021-09-30 17:14:50 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup , find_packages
+
+setup(
+    name = "src",
+    version = "0.0.1",
+    description = "its an insurance predictor package",
+    author="charanraj2411",
+    packages=find_packages(),
+    license="MIT"
+)
